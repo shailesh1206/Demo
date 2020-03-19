f8bf30d4bc41b3cd1515dfe81ca3460206d2ff11
shailesh sharad chaudhari <57498959+shailesh1206@users.noreply.github.com>
2020-03-19 17:34:18 +0530

    first task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { FormGroup  ,FormBuilder, FormControl,FormsModule  , Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+@NgModule(
+  {
+    declarations: [],
+    imports: [ReactiveFormsModule, FormControl, FormBuilder ,FormGroup, Validators],
+    providers: [],
+    exports: [FormsModule,ReactiveFormsModule],
+  }
+)
 export class AppComponent {
   title = 'DEMOS';
 }
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core'; 
+import {ReactiveFormsModule,FormsModule, FormControl, FormBuilder ,FormGroup, Validators} from '@angular/forms';  
+import { NgModule } from '@angular/core';
+
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+
+
+
+export class FormComponent implements OnInit {
+
+    registerForm: FormGroup;
+    submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group(
+      {
+        fname:['',Validators.required],
+        age: ['',Validators.required],
+        mno:['',Validators.required, Validators.minLength[10]],
+        password: ['',Validators.required,Validators.minLength[6]],
+        acceptTerms: [false, Validators.requiredTrue]
+      
+      },
+      );
+  }
+
+  get f()
+  {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if(this.registerForm.invalid)
+    {
+      return;
+    }
+
+    alert('SUCCESS !! :- \n\n' +JSON.stringify(this.registerForm.value, null,4 ));
+
+  }
+
+  OnReset()
+  {
+    this.submitted = false;
+    this.registerForm.reset();
+  }
+
+}
